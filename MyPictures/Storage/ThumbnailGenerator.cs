22c9a7da595c12f845e46ffbc2dc98878e3015c4
John <adam@saunte.dk>
2019-04-03 12:04:51 +0200

    Edit thumbnail generator to comply with 1:1 ratio

--- MyPictures/Storage/ThumbnailGenerator.cs
+++ MyPictures/Storage/ThumbnailGenerator.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -20,14 +21,23 @@ namespace MyPictures.Storage
             // Scale values for width and height - ensures 1:1 ratio
             double scaleWidth = 300d / frame.PixelWidth;
             double scaleHeight = 300d / frame.PixelHeight;
-   
+            double scale = (scaleWidth > scaleHeight) ? scaleHeight : scaleWidth;
+
             // Transform the picture
-            ScaleTransform transform = new ScaleTransform(scaleWidth, scaleHeight);
-            TransformedBitmap transformed = new TransformedBitmap(frame, transform);
+            // https://stackoverflow.com/questions/22013835/how-to-effectively-crop-and-scale-image-data
+            ScaleTransform transform = new ScaleTransform(scale, scale); //(scaleWidth, scaleHeight);
+            double size = (frame.PixelHeight > frame.PixelWidth) ? frame.PixelWidth : frame.PixelHeight;
+            int startX = (frame.PixelWidth == (int)size) ? 0 : (int)((frame.PixelWidth/ 2) - (0.5* size));
+            int startY = (frame.PixelHeight == (int)size) ? 0 : (int)((frame.PixelHeight / 2) - (0.5*size));
+
+            // Crop picture and create new transformation, with 1:1 ratio
+            CroppedBitmap crop = new CroppedBitmap(frame, new Int32Rect(startX, startY, (int)size, (int)size));
+            TransformedBitmap transformed = new TransformedBitmap(crop, transform);
 
             // Return the transformation as a bitmapframe
             return BitmapFrame.Create((BitmapSource)transformed);
         }
+
 
     }
 }
